694a32894468e1f6d760e3c935185cdc4846fec9
Alfredo Roca <alfredo.roca.mas@gmail.com>
2015-09-06 14:07:11 +0200

    added association between task and subtasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,6 +12,7 @@
 class Task < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 	has_many :activities
+  has_many :subtasks
   has_many :users, -> { uniq }, through: :activities
   default_scope -> { order(:name) }
   scope :how_many_new_activity, -> { limit(5) }
