4eba2235a26b52ad454af025ca6eba906d4d69ff
Alfredo Roca <alfredo.roca.mas@gmail.com>
2016-06-11 13:57:00 +0200

    adds long duration conversion to string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,14 +23,15 @@ module ApplicationHelper
   end
 
   def long_duration_to_s(total_seconds)
-    total_seconds ||= 0
-    seconds = total_seconds % 60
-    minutes = (total_seconds / 60) % 60
-    hours = total_seconds / (60 * 60)
-    days = total_seconds / (60 * 60 * 24)
+    total_seconds = total_seconds.to_i
 
-    # format("%02d:%02d:%02d", hours, minutes, seconds)
-  	format("%02d:%02d", hours, minutes)
+    days  = total_seconds/86400
+    remaining_seconds_after_days = total_seconds - days*86400
+    hours = remaining_seconds_after_days/3600
+    remaining_seconds_after_hours = remaining_seconds_after_days - hours*3600
+    minutes = remaining_seconds_after_hours/60
+
+  	format("%02dd %02dh %02dm", days, hours, minutes)
   end
 
 end
