0736bbb98401c838b532f55683cee02277a5ca52
Alfredo Roca <alfredo.roca.mas@gmail.com>
2016-06-11 18:00:39 +0200

    changes format to hours of gittag activity duration calculation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,15 +23,7 @@ module ApplicationHelper
   end
 
   def long_duration_to_s(total_seconds)
-    total_seconds = total_seconds.to_i
-
-    days  = total_seconds/86400
-    remaining_seconds_after_days = total_seconds - days*86400
-    hours = remaining_seconds_after_days/3600
-    remaining_seconds_after_hours = remaining_seconds_after_days - hours*3600
-    minutes = remaining_seconds_after_hours/60
-
-  	format("%02dd %02dh %02dm", days, hours, minutes)
+    (total_seconds/3600).ceil.to_s + "h"
   end
 
 end
