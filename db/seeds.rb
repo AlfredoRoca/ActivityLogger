5ed7887c449656ba8b0e14922cbfda31b18c52ea
Alfredo Roca <alfredo.roca.mas@gmail.com>
2017-01-27 20:30:58 +0100

    fixes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-User.create name: "Alfredo", email: "alfredo.roca.mas@gmail.com", admin: true, password: "123123123", password_confirmation: "123123123"
+admin = User.new name: "Alfredo", email: "alfredo.roca.mas@gmail.com", admin: true, password: "123123123", password_confirmation: "123123123"
+admin.skip_confirmation!
+admin.save
